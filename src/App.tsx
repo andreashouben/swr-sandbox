554ac283169d31d11d2d32c86f74adbf9b7fe93c
Andreas Houben <andreas.houben@codecentric.de>
2022-01-14 09:31:44 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg='light' variant={'light'}>
+                <Container>
+                    <Navbar.Brand>SWR-Sandbox</Navbar.Brand>
+                    <Nav className={"me-auto"}>
+                        <Nav.Link href='/'>Simple Demo</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {worker} from './mocks/browser';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 worker.start({onUnhandledRequest:'bypass'})
 
